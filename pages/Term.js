73cd74c263a4a3fde9170f1f98b70edcee4ab81e
Denischehov <ivanhram511@gmail.com>
2024-05-03 02:51:09 +0800

    modified:   pages/Term.js

--- pages/Term.js
+++ pages/Term.js
@@ -9,49 +9,15 @@ export default function Term() {
 			className=' absolute top-0 left-0 w-full h-full  lg:px-48 px-16'
 		>
 			<div className='text-center my-32 lg:my-32 md:my-48 sm:my-72 '>
-				<h2 className='border-2 rounded-xl p-1 text-3xl text-blue-200 border-blue-400'>
-					Срок/Сумма кредита
+				<h2 className='rounded-xl p-1 text-3xl  border-[1px] p-2 hover:text-indigo-200 my-3  hover:border-x-indigo-200 hover:border-y-indigo-400 border-y-gray-500 border-x-gray-200'>
+					В РАЗРАБОТКЕ
 				</h2>
-				<p className='border-2 my-3 border-blue-400 rounded-xl text-xl'>
-					<h2 className='text-2xl text-blue-300'>Без заявки</h2> Добрый день!
-					И.О
-					<br />
-					Звонок из Совкомбанка, меня зовут ..! Звоню, чтобы расказать вам о
-					выгодных предложениях от нашего банка и сориентировать по доступным
-					продуктам. Для этого задам вам несколько простых вопросов
-				</p>
 
-				<p className='border-2 my-3 border-blue-400 rounded-xl text-xl'>
-					<h2 className='text-3xl text-blue-300'>
-						{' '}
-						<a
-							target='_blank'
-							rel='noreferrer'
-							href='https://minerva.sovcombank.ru/content/space/52/article/30148/page/3?query=%D1%85%D0%B0%D0%BB%D0%B2%D0%B0'
-						>
-							Заявка КХ Рефинансирование
-						</a>
-					</h2>
-					Добрый день! И.О? <br />
-					Звонок из Совкомбанка, меня зовут ..! Понадобиться всего пару минут
-					вашего времени! Звоню по вашей заявке на карту Халва, которую вы
-					оставили на сайте нашего банка, Планируете рефинансировать Халвой
-					карты других банков, верно?
-				</p>
-
-				<p className='border-2 my-3 border-blue-400 rounded-xl text-xl'>
-					<h2 className='text-3xl text-blue-300'>Кредитная карта (плюс)</h2>{' '}
-					Добрый день! И.О? <br />
-					Звонок из Совкомбанка, меня зовут ..! Понадобиться всего пару минут
-					вашего времени! Звоню по вашей заявке на новую кредитную карту от
-					совком банка 180 дней без %, сейчас раскажу о преимуществах карты а
-					далее сможем перейти к оформлению, хорошо?
-				</p>
-				<footer className=' mt-6  border-2 rounded-xl border-blue-400 text-center p-3'>
+				<footer className=' mt-6  rounded-xl p-1  border-[1px] p-2  my-3  border-y-gray-500 border-x-gray-200'>
 					<p>
 						Разработчик: Иван Дмитриевич. Контакты:
 						<a
-							className='hover:text-blue-300 duration-300 px-2'
+							className='hover:text-indigo-300 duration-300 px-2'
 							href='https://t.me/Freak_3112'
 							target='_blank'
 							rel='noreferrer'
@@ -59,7 +25,7 @@ export default function Term() {
 							Telegram
 						</a>
 						<a
-							className='hover:text-blue-300 duration-300'
+							className='hover:text-indigo-300 duration-300'
 							href='https://vk.com/fckallworldexpectyou'
 							target='_blank'
 							rel='noreferrer'
@@ -67,7 +33,7 @@ export default function Term() {
 							Вконтакте
 						</a>
 						<a
-							className='hover:text-blue-300 duration-300 px-2'
+							className='hover:text-indigo-300 duration-300 px-2'
 							target='_blank'
 							rel='noreferrer'
 							href='https://mail.google.com/mail/u/1/#inbox?compose=CllgCJvnrFxthbvVSqjmbwKlLVMNgjbQCCShwHPKXZTCSLLhsvlmgWrsmCFSzjZPlgLGvThchqB'
